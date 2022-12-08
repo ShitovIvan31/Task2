98fe6823f98436ff7702742fcc8d5f9e35675bc6
Shitov Ivan <solnishko313131@gmail.com>
2022-12-08 10:13:49 +0500

    Task2

--- src/main/java/Repository/ForProductRepository.java
+++ src/main/java/Repository/ForProductRepository.java
@@ -1,9 +1,6 @@
 package Repository;
 
 import Content.Product;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,16 +8,9 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 
-@Configuration
-@ComponentScan
+@Component
 public class ForProductRepository extends ProductRepository {
     private List<Product> products;
-
-    @Bean
-    public ForProductRepository forProductRepository () {
-        return new ForProductRepository();
-    }
-
     public List<Product> getAllProducts() {
         return products;
     }
